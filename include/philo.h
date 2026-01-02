2f8394485134698a36b5ed79cd25dc1e4f0ff58d
Maksym <maksymchiachakov@gmail.com>
2026-01-02 10:58:45 +0100

    add sommes comments in philo.h

--- include/philo.h
+++ include/philo.h
@@ -16,38 +16,41 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <pthread.h>
-# include <sys/time.h>
+# include <pthread.h> // Наша бібліотека потоків 
+# include <sys/time.h> // Бібліотека для роботи з часом
 
-typedef struct s_data	t_data;
+typedef struct s_data	t_data; // Прототип для використання у структурі s_philo
 
+// Створення структури нашого філософа
 typedef struct s_philo
 {
-	int				id;
-	int				meals_eaten;
-	long			last_meal;
-	pthread_t		thread;
-	pthread_mutex_t	*left_fork;
-	pthread_mutex_t	*right_fork;
-	t_data			*data;
+	int				id; // Його ідентифікатор
+	int				meals_eaten; // Кількість з'їджених порцій
+	long			last_meal; // Останній прийом їжі
+	pthread_t		thread; // Потік (нитка)
+	pthread_mutex_t	*left_fork; // Ліва виделка
+	pthread_mutex_t	*right_fork; // Права виделка
+	t_data			*data; // Та наша Data
 }	t_philo;
 
+// Створення нашої data
 typedef struct s_data
 {
-	int				nb_philo;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				nb_meals;
-	int				dead;
-	long			start_time;
-	pthread_mutex_t	*forks;
-	pthread_mutex_t	print;
-	pthread_mutex_t	death;
-	pthread_mutex_t	meal;
-	t_philo			*philos;
+	int				nb_philo; // Кількість філософів
+	int				time_to_die; // Час до смерті
+	int				time_to_eat; // Час до прийому їжі
+	int				time_to_sleep; // Час для сну
+	int				nb_meals; // Кількість порцій (якщо є у 5-му аргументі (av[5]))
+	int				dead; // Змінна смерті
+	long			start_time; // Початковий час
+	pthread_mutex_t	*forks; // Список (масив) виделок
+	pthread_mutex_t	print; // Поток print
+	pthread_mutex_t	death; // Поток death
+	pthread_mutex_t	meal; // Поток meal
+	t_philo			*philos; // Та наші філософи
 }	t_data;
 
+// Всі наші функції
 int		ft_atoi(const char *str);
 int		init_data(t_data *data, int ac, char **av);
 int		init_philos(t_data *data);
